0e8bb5b1c1d7fb65d857a6d5de051e5816cffd1d
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2022-05-11 22:09:53 +0100

    Install Bootstrap and organise images
    Images were put into cards so they all have the same size. They are divided in two columns.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Card, Row, Col, Container } from "react-bootstrap";
+import "./App.css";
 
 const App = () => {
   const [images, setImages] = useState();
 
   useEffect(() => {
-    fetch('images?limit=10')
-      .then(res => res.json())
-      .then(data => {
-        console.log('Success:', data);
+    fetch("images?limit=10")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Success:", data);
         setImages(data);
       })
-      .catch(error => {
-        console.error('Error:', error);
+      .catch((error) => {
+        console.error("Error:", error);
       });
   }, []);
 
   return (
-    <div className='app'>
-      {
-        images && images.map(img => (
-          <div key={img.id} >
-            <img src={`${img.url}.jpg`} alt=''/>
-            <img src={`${img.user.profile_image}.webp`} alt=''/>
-          </div>
-        ))
-      }
-    </div>
+    <Container>
+      <div className="app">
+        {images &&
+          images.map((img) => (
+            <div key={img.id}>
+              <Row className="row justify-content-center">
+                <Col md={4} style={{ margin: "1em" }}>
+                  <Card className="card">
+                    <img className="image" src={`${img.url}.jpg`} alt="" />
+                  </Card>
+                </Col>
+
+                <Col md={4} style={{ margin: "1em" }}>
+                  <Card className="card">
+                    <img
+                      className="image card-img-center"
+                      src={`${img.user.profile_image}.webp`}
+                      alt=""
+                    />
+                  </Card>
+                </Col>
+              </Row>
+            </div>
+          ))}
+      </div>
+    </Container>
   );
-}
+};
 
 export default App;
